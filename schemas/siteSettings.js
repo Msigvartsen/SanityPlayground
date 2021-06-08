eb720a9c7707f50c546a9517ac47eadeec5b26a5
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2021-06-08 12:11:07 +0200

    Add sitesettings singleton document using structurebuilder

---
+++ schemas/siteSettings.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'siteSettings',
+    title: 'Site Settings',
+    type: 'document',
+    fields: [
+      {
+        name: 'title',
+        title: 'Site Title',
+        type: 'string',
+      },
+      {
+          name: 'description',
+          title: 'Meta Description',
+          type: 'text'
+      }
+    ]
+  }
+  
---
+++ src/deskStructure.js
@@ -0,0 +1,19 @@
+// /deskStructure.js
+import S from '@sanity/desk-tool/structure-builder'
+
+export default () =>
+  S.list()
+    .title('Base')
+    .items(
+        [
+        S.listItem()
+        .title('Settings')
+        .child(
+            S.document()
+            .schemaType('siteSettings')
+            .documentId('siteSettings')
+        ),
+        S.divider(),
+      ...S.documentTypeListItems().filter(item => !['siteSettings'].includes(item.getId()))
+    ]
+)
