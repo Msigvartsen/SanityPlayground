c584f1f1a2605f9efd05b6ef8e179040ea869062
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2021-06-08 11:05:50 +0200

    initial commit

---
+++ logo/myLogo.js
@@ -0,0 +1,10 @@
+// ./logo/myLogo.js
+
+import React from 'react';
+import styles from './myLogo.css'
+
+const myLogo = () => (
+  <img className={styles.myLogo} src="/static/NC_logo_turkis.png" alt="Watch drawing"/>
+);
+
+export default myLogo;
