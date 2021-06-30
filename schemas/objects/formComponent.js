f487587866a1449e9915fb618ebd9ac871ec2184
Magnus Sigvartsen <magnus.sigvartsen@gmail.com>
2021-06-30 07:47:00 +0200

    update sanity studio

---
+++ schemas/objects/formComponent.js
@@ -0,0 +1,12 @@
+export default {
+    name: 'inputFormComponent',
+    title: 'Main Navigation',
+    type: 'object',
+    fields: [
+      {
+        name: 'navigation',
+        title: 'Navigation',
+        type: 'string',
+      }
+    ]
+}
